3bc8fa8505dbb70384300bf4804d734b395f3865
Ali Rahimpour <li.Rahimpour@gmail.com>
2023-03-12 19:31:57 +0100

    push the initial module intervisuell

---
+++ intervisuell/src/main/java/com/p0006/intervisuell/IntervisuellApplication.java
@@ -0,0 +1,13 @@
+package com.p0006.intervisuell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntervisuellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntervisuellApplication.class, args);
+    }
+
+}
---
+++ intervisuell/src/test/java/com/p0006/intervisuell/IntervisuellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p0006.intervisuell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntervisuellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
